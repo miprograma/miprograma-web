de973abb58a2d1e2033672e4a3d2b0992480c869
Lucas <lucas@MacBook-Air-de-Lucas.local>
2019-04-12 21:17:45 +0200

    Updates

---
+++ precache-manifest.87b1b18c8f0eac36f65f0736f5e71852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2d8c3f418abb797a1797f0f6bbcdbf8",
+    "url": "/miprograma-web/static/media/161118Cardamomo Esp728x90px.d2d8c3f4.jpg"
+  },
+  {
+    "revision": "de80dad72145ccbba8cd",
+    "url": "/miprograma-web/static/js/runtime~main.e5aa9649.js"
+  },
+  {
+    "revision": "254340a38b7e14333635",
+    "url": "/miprograma-web/static/js/main.220b5503.chunk.js"
+  },
+  {
+    "revision": "6efa520acff8b25046c9",
+    "url": "/miprograma-web/static/js/2.52302b1c.chunk.js"
+  },
+  {
+    "revision": "254340a38b7e14333635",
+    "url": "/miprograma-web/static/css/main.378f3f3d.chunk.css"
+  },
+  {
+    "revision": "6efa520acff8b25046c9",
+    "url": "/miprograma-web/static/css/2.50e0dc8a.chunk.css"
+  },
+  {
+    "revision": "b660c8fecebc71e061a23104439b392d",
+    "url": "/miprograma-web/index.html"
+  }
+];
