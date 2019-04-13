3225393b30c06ebebbf428f8067bd26ec50a7390
Lucas <lucas@MacBook-Air-de-Lucas.local>
2019-04-13 11:03:03 +0200

    back to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import 'antd-mobile/dist/antd-mobile.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>, document.getElementById('root'));
+  </BrowserRouter>, document.getElementById('root'));
 
 
 serviceWorker.unregister();
